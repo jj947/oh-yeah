adf791a55f2f8b34dae31397411bcf8dada14b91
jj947 <djoulian.carro37@gmail.com>
2026-02-05 09:56:51 +0100

    Create client.py

---
+++ client.py
@@ -0,0 +1,24 @@
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("Connecté au serveur")
+
+@sio.on("status")
+def status(msg):
+    print(msg)
+
+@sio.on("message")
+def message(msg):
+    print("💬", msg)
+
+emotion = input("Emotion : ")
+
+sio.connect("https://oh-yeah-1.onrender.com")
+sio.emit("join", {"emotion": emotion})
+
+while True:
+    msg = input()
+    sio.send(msg)
