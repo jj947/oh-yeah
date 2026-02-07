425c7f2dd6fb1026899629b2a1791715eb0521f3
jj947 <djoulian.carro37@gmail.com>
2026-02-07 11:38:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import eventlet
 eventlet.monkey_patch()
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+users = {}
+
+@socketio.on("join")
+def handle_join(data):
+    pseudo = data["pseudo"]
+    emotion = data["emotion"]
+
+    users[request.sid] = {
+        "pseudo": pseudo,
+        "emotion": emotion
+    }
+
+    emit("message", f"🟢 {pseudo} a rejoint ({emotion})", broadcast=True)
 
 @app.route("/")
 def index():
@@ -13,8 +26,20 @@ def index():
 
 @socketio.on("message")
 def handle_message(data):
-    text = f"[{data['emotion']}] {data['text']}"
-    emit("message", text, broadcast=True)
+    user = users.get(request.sid)
+    if not user:
+        return
+
+    pseudo = user["pseudo"]
+    emotion = user["emotion"]
+    text = data["text"]
+
+    emit(
+        "message",
+        f"[{pseudo} | {emotion}] {text}",
+        broadcast=True
+    )
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
+
