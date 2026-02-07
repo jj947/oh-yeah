ea5de4f96da73fb6d1a348a0e16eb527fd296f7b
jj947 <djoulian.carro37@gmail.com>
2026-02-07 11:18:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, join_room, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    async_mode="threading"
-)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@socketio.on("join")
-def on_join(data):
-    emotion = data["emotion"]
-    join_room(emotion)
-    emit("message", {"message": f"Quelqu’un a rejoint : {emotion}"}, room=emotion)
-
 @socketio.on("message")
 def handle_message(data):
-    emit("message", data, broadcast=True)
+    text = f"[{data['emotion']}] {data['text']}"
+    emit("message", text, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
