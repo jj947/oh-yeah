0f7f2453e5924957dc38ccdec89c225e9cad7ed1
jj947 <djoulian.carro37@gmail.com>
2026-02-07 11:27:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 @app.route("/")
 def index():
@@ -15,3 +18,4 @@ def handle_message(data):
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
+
