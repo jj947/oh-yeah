3b913bf6e8ba0ff12344b6e7718cb45bbc425950
jj947 <djoulian.carro37@gmail.com>
2026-02-08 12:23:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,126 +1,100 @@
-from flask import Flask, request, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
+import uuid
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# -------------------------
-# ÉTAT GLOBAL
-# -------------------------
+MAX_PER_ROOM = 2
 
-users = {}  # sid -> {pseudo, emotion, partner}
+waiting_queue = []          # sockets en attente
+rooms = {}                  # room_id -> [sid, sid]
+user_room = {}              # sid -> room_id
+user_pseudo = {}            # sid -> pseudo
 
-waiting = {
-    "triste": [],
-    "content": [],
-    "colere": [],
-    "stresse": []
-}
 
-# -------------------------
-# MATCHING
-# -------------------------
-
-def try_match(emotion):
-    while len(waiting[emotion]) >= 2:
-        a = waiting[emotion].pop(0)
-        b = waiting[emotion].pop(0)
-
-        users[a]["partner"] = b
-        users[b]["partner"] = a
-
-        emit("message", "🟢 Partenaire trouvé !", room=a)
-        emit("message", "🟢 Partenaire trouvé !", room=b)
-
-# -------------------------
-# CONNEXION
-# -------------------------
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
+
+def try_create_room():
+    """Crée un salon si possible"""
+    if len(waiting_queue) >= 2:
+        sid1 = waiting_queue.pop(0)
+        sid2 = waiting_queue.pop(0)
+
+        room_id = str(uuid.uuid4())[:8]
+        rooms[room_id] = [sid1, sid2]
+
+        user_room[sid1] = room_id
+        user_room[sid2] = room_id
+
+        join_room(room_id, sid=sid1)
+        join_room(room_id, sid=sid2)
+
+        socketio.emit("system", "Salon créé, vous êtes connectés 🎉", room=room_id)
+
+
 @socketio.on("join")
-def join(data):
-    sid = request.sid
-    pseudo = data["pseudo"]
-    emotion = data["emotion"]
+def handle_join(data):
+    pseudo = data.get("pseudo", "Anonyme")
+    user_pseudo[request.sid] = pseudo
 
-    users[sid] = {
-        "pseudo": pseudo,
-        "emotion": emotion,
-        "partner": None
-    }
+    waiting_queue.append(request.sid)
+    emit("system", "En attente d’un autre utilisateur…")
 
-    waiting[emotion].append(sid)
-    emit("message", "⏳ En attente d’un partenaire...", room=sid)
+    try_create_room()
 
-    try_match(emotion)
-
-# -------------------------
-# MESSAGE
-# -------------------------
 
 @socketio.on("message")
 def handle_message(data):
-    sid = request.sid
-    user = users.get(sid)
-
-    if not user:
+    room_id = user_room.get(request.sid)
+    if not room_id:
         return
 
-    text = data["text"]
-    pseudo = user["pseudo"]
-    emotion = user["emotion"]
-    partner = user["partner"]
+    emit(
+        "message",
+        {
+            "pseudo": user_pseudo.get(request.sid, "???"),
+            "text": data["text"]
+        },
+        room=room_id
+    )
 
-    msg = f"[{pseudo} | {emotion}] {text}"
-
-    # afficher chez soi
-    emit("message", msg, room=sid)
-
-    # envoyer au partenaire
-    if partner:
-        emit("message", msg, room=partner)
-
-# -------------------------
-# DÉCONNEXION
-# -------------------------
 
 @socketio.on("disconnect")
-def disconnect():
+def handle_disconnect():
     sid = request.sid
-    user = users.get(sid)
 
-    if not user:
-        return
+    # Retirer de la file d’attente
+    if sid in waiting_queue:
+        waiting_queue.remove(sid)
 
-    emotion = user["emotion"]
-    partner = user["partner"]
+    # Si dans un salon
+    room_id = user_room.get(sid)
+    if room_id and room_id in rooms:
+        rooms[room_id].remove(sid)
 
-    # retirer de la file d'attente si besoin
-    if sid in waiting[emotion]:
-        waiting[emotion].remove(sid)
+        emit("system", "L’autre utilisateur a quitté le salon ❌", room=room_id)
 
-    # gérer le partenaire
-    if partner and partner in users:
-        users[partner]["partner"] = None
-        waiting[emotion].append(partner)
+        # Si salon vide → supprimer
+        if not rooms[room_id]:
+            del rooms[room_id]
+        else:
+            # remettre le survivant en attente
+            survivor = rooms[room_id][0]
+            del rooms[room_id]
+            del user_room[survivor]
 
-        emit(
-            "message",
-            "🔴 Ton partenaire a quitté. En attente d’un nouveau...",
-            room=partner
-        )
+            waiting_queue.append(survivor)
+            emit("system", "Retour en attente d’un nouveau partenaire…", to=survivor)
+            try_create_room()
 
-        try_match(emotion)
+    user_room.pop(sid, None)
+    user_pseudo.pop(sid, None)
 
-    del users[sid]
-
-# -------------------------
-# LANCEMENT
-# -------------------------
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
-
+    socketio.run(app, debug=True)
