86974a8f17c32bd1fb683470fc87b465820ae645
jj947 <djoulian.carro37@gmail.com>
2026-02-16 22:18:05 +0100

    Update menu.js

--- static/menu.js
+++ static/menu.js
@@ -1,1 +1,14 @@
+function goChat() {
+    const username = document.getElementById("username").value;
+    const emotion = document.getElementById("emotion").value;
 
+    if (!username || !emotion) {
+        alert("Complète tout !");
+        return;
+    }
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("emotion", emotion);
+
+    window.location.href = "/chat";
+}
