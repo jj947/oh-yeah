5b669ca179b3f1cc0ab948be55bc9b71bec65b1d
jj947 <djoulian.carro37@gmail.com>
2026-02-16 22:56:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    async_mode="eventlet"
+)
 
 # --- états ---
 waiting_users = {}     # emotion -> sid
@@ -104,6 +108,7 @@ def handle_disconnect():
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
+
 
 
 
