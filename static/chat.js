eb8eb4b6cd918f9e08d769a2b1565027dffc445e
jj947 <djoulian.carro37@gmail.com>
2026-02-17 11:33:44 +0100

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,1 +1,17 @@
+function sendMessage() {
+    const input = document.getElementById("messageInput");
+    const msg = input.value.trim();
+    if (!msg) return;
 
+    socket.emit("send_message", { message: msg });
+    input.value = "";
+}
+
+socket.on("receive_message", data => {
+    const div = document.createElement("div");
+    div.textContent = `${data.from} : ${data.message}`;
+    document.getElementById("messages").appendChild(div);
+
+    const messages = document.getElementById("messages");
+    messages.scrollTop = messages.scrollHeight;
+});
