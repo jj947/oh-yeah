01bdc6173491b91c0a56e37477ada27b356f64bc
jj947 <djoulian.carro37@gmail.com>
2026-05-06 21:41:56 +0200

    Create serveur.py

---
+++ serveur.py
@@ -0,0 +1,9 @@
+import eventlet
+eventlet.monkey_patch()
+
+from app import app, socketio
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
