cddc444cd66fded9496d920fac14a003cd9fd47c
jj947 <djoulian.carro37@gmail.com>
2026-05-06 20:14:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-
+import os
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret-key"
 
@@ -108,5 +108,8 @@ def handle_disconnect():
 
 
 # ===== LANCEMENT =====
+
+
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
