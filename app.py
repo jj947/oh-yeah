15ef8877da62bb0ce7199164c8adcbefea0732c6
jj947 <djoulian.carro37@gmail.com>
2026-05-06 21:37:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import os
 import eventlet
 eventlet.monkey_patch()
-
+import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from collections import deque
