253e87d3f5e59bf3ef867512ece4646d1914ede5
jj947 <djoulian.carro37@gmail.com>
2026-05-14 11:13:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, Response
 from flask_socketio import SocketIO, emit
 from collections import deque
 import os
